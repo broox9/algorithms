014a7f2df1db392527af4ed29a32a2aabf500af8
Stephens, Brookes <Brookes.Stephens@priceline.com>
2016-08-28 17:48:31 -0400

    oops

--- server/week3/randomized-selection.js
+++ server/week3/randomized-selection.js
@@ -1,6 +1,32 @@
+//Randomized Selection
 function RSelect (arr, len, nth) {
   //base case
-  if (arr.length === 1) {
+  if (len === 1) {
     return arr[0];
   }
+
+  //choose pivot array, uniformly at random 
+  //(the main point of the analysis is that random pivots work well enough)
+
+  let new_index;  //new_index = partition arr around pivot
+
+  let j = new_index;
+
+  if (j == nth) {
+    //jackpot
+    return arr[j]
+  }
+
+  //hardest case
+  if (j > nth) {
+    //partition on the left of the pivot
+    //still looking for nth//
+    //arr length is j -1 
+    RSelect(arr[0] ... arr[j-1], (j-1), nth)
+  }
+
+  if (j < nth ) {
+    RSelect(arr[new_pivot] ... ar[len - 1], (len - j), (nth - j))
+  }
+
 }
